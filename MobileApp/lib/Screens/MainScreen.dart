8b70c94cbadd53d824a1ddad9c4ae6e95e5cfb62
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-10-24 19:43:48 +0200

    Set min folder and screens
    Co-Authored-By: Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>

---
+++ MobileApp/lib/Screens/MainScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+class MainUserScreen extends StatelessWidget{
+
+@override
+Widget build(BuildContext context){
+
+return Scaffold(
+  body: Column(
+    children: [
+      Expanded(flex: 1,child: Container(
+        color: Colors.brown[50],
+      ) 
+      ),
+    ],
+),
+); 
+
+
+}
+
+
+}
---
+++ MobileApp/lib/app.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:helloflutter/Screens/MainScreen.dart';
+
+class App extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'AssignmentOne',
+    theme: ThemeData(
+      primarySwatch: Colors.brown,
+      
+    ),
+    home: MainUserScreen() ,
+   );
+
+
+
+  }
+
+
+}
