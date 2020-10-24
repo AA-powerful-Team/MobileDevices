963ab8bfb5bf94dce692250cb5062574941d73c8
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-10-24 22:22:33 +0200

    estructure of BG with containers and columns
    Co-Authored-By: Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>

--- MobileApp/lib/Screens/MainScreen.dart
+++ MobileApp/lib/Screens/MainScreen.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
 
-
-
-class MainUserScreen extends StatelessWidget{
-
-@override
-Widget build(BuildContext context){
-
-return Scaffold(
-  body: Column(
-    children: [
-      Expanded(flex: 1,child: Container(
-        color: Colors.brown[50],
-      ) 
+class MainUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column( //Column One : STRUCTURE OF THE WHOLE SCREEN
+        children: [
+          Expanded(
+            flex: 2,
+            child: BaseBackground(),
+          ),
+        ],
       ),
-     Container(
-        alignment: Alignment.bottomCenter,
-        width: 100,
-        height: 200,
-         )
-      
-    ],
-),
-); 
-
-
+    );
+  }
 }
 
+class BaseBackground extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Column( //Column Two
+        children: [
+          Expanded( 
+            flex: 3,
+            child: UpperSegmentBGB(),//invisible Rect
+          ),
+          Expanded(
+            flex: 1,
+            child: LowerSegmentBGB(),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class LowerSegmentBGB extends StatelessWidget { //Lower part of the background base(BGB)
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container(//Green Rect
+        decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.only(
+        bottomRight: Radius.circular(30),
+        bottomLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+        topLeft: Radius.circular(30),
+      ),
+    ));
+  }
+}
+
+class UpperSegmentBGB extends StatelessWidget { //Upper part of the background base(BGB)
+ 
+   @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
