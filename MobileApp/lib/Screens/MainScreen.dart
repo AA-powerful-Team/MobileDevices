8468aee4babd1fc0e6f320b370298295832c9b26
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-10-25 16:37:25 +0100

    Added another child to the lower segment

--- MobileApp/lib/Screens/MainScreen.dart
+++ MobileApp/lib/Screens/MainScreen.dart
@@ -4,7 +4,8 @@ class MainUserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column( //Column One : STRUCTURE OF THE WHOLE SCREEN
+      body: Column(
+        //Column One : STRUCTURE OF THE WHOLE SCREEN
         children: [
           Expanded(
             flex: 2,
@@ -17,16 +18,16 @@ class MainUserScreen extends StatelessWidget {
 }
 
 class BaseBackground extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
-      child: Column( //Column Two
+      child: Column(
+        //Column Two
         children: [
-          Expanded( 
+          Expanded(
             flex: 3,
-            child: UpperSegmentBGB(),//invisible Rect
+            child: UpperSegmentBGB(), //invisible Rect
           ),
           Expanded(
             flex: 1,
@@ -38,26 +39,56 @@ class BaseBackground extends StatelessWidget {
   }
 }
 
-class LowerSegmentBGB extends StatelessWidget { //Lower part of the background base(BGB)
- 
+class LowerSegmentBGB extends StatelessWidget {
+  //Lower part of the background base(BGB)
+
   @override
   Widget build(BuildContext context) {
-    return Container(//Green Rect
-        decoration: BoxDecoration(
+    return Container(
+      //Green Rect
+      decoration: BoxDecoration(
         color: Colors.green,
         borderRadius: BorderRadius.only(
-        bottomRight: Radius.circular(30),
-        bottomLeft: Radius.circular(30),
-        topRight: Radius.circular(30),
-        topLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+          bottomLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
       ),
-    ));
+
+      child: Container( //Inside the the lower part segmentBGB
+  
+        padding: EdgeInsets.all(40), //HARDCODED
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row( //Text row
+              children: [
+                Text(
+                  'Recently Assigned', //HARDCODED
+                  style: TextStyle(
+                    fontFamily: '',
+                    fontSize: 25, //HARDCODED
+                  ),
+                ),
+                SizedBox(width: 150),//HARDCODED
+                Text(
+                  'All tasks',
+                  style: TextStyle(fontFamily: '', fontSize: 15),//HARDCODED
+                ), 
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
-class UpperSegmentBGB extends StatelessWidget { //Upper part of the background base(BGB)
- 
-   @override
+class UpperSegmentBGB extends StatelessWidget {
+  //Upper part of the background base(BGB)
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
