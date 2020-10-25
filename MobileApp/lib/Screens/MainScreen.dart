c143edd14180a131a50ddad82dccdd9b39fff309
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-10-25 19:38:23 +0100

    Organized new elements as widgets
    Co-Authored-By: Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>

--- MobileApp/lib/Screens/MainScreen.dart
+++ MobileApp/lib/Screens/MainScreen.dart
@@ -66,140 +66,164 @@ class LowerSegmentBGB extends StatelessWidget {
           children: [
             Expanded(
               flex: 2,
-              child: Row(
-                //Text row type of task
-                children: [
-                  Text(
-                    'Recently Assigned', //HARDCODED
-                    style: TextStyle(
-                      fontFamily: '',
-                      fontSize: 25, //HARDCODED
-                    ),
-                  ),
-                  SizedBox(width: 150), //HARDCODED
-                  Spacer(),
-                  Text(
-                    'All tasks',
-                    style: TextStyle(fontFamily: '', fontSize: 15), //HARDCODED
-                  ),
-                ],
-              ),
+              child: LowerSegmentTitle(),
             ),
             Expanded(
               flex: 4, //middlebox
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.orange[600],
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(25),
-                          topLeft: Radius.circular(25),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 20,
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(25),
-                          topRight: Radius.circular(25),
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          Expanded(
-                              flex: 2,
-                              child: Row(
-                                children: [
-                                  Text(
-                                    'Task name', //HARDCODED
-                                    style: TextStyle(
-                                      fontFamily: '',
-                                      fontSize: 25, //HARDCODED
-                                    ),
-                                  ),
-                                  SizedBox(width: 150), //HARDCODED
-                                  Spacer(),
-                                  Text(
-                                    'priority',
-                                    style: TextStyle(
-                                        fontFamily: '',
-                                        fontSize: 15), //HARDCODED
-                                  ),
-                                ],
-                              )),
-                               SizedBox(height: 15,),
-                          Expanded(
-                            flex: 4,
-                            child: Align(alignment: Alignment.topLeft ,child: Text(
-                                'lorem ipsume fhjkasfjhnajhfjshnjcnasndjncansjncnajsndj',
-                                style: TextStyle(
-                                  fontFamily: '',
-                                  fontSize: 15,
-                                ),
-                               
-                                ),
-                                )
-                              
-                          ),
-                          Divider(height: 20),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              child: Row(
-                                children: [
-                                  Icon(Icons.timelapse),
-                                  SizedBox(width:20),
-                                  Text('xx/xx/xxxx'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+              child: TaskInformation(),
             ),
             SizedBox(height: 15),
             Expanded(
                 flex: 1, // icons bar
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                      size: 50,
-                    ),
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                      size: 50,
-                    ),
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                      size: 50,
-                    ),
-                    Icon(
-                      Icons.home,
-                      color: Colors.grey,
-                      size: 50,
-                    ),
-                  ],
-                ))
+                child: Appbar())
           ],
         ),
       ),
+    );
+  }
+}
+
+class Appbar extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.home,
+          color: Colors.grey,
+          size: 50,
+        ),
+        Icon(
+          Icons.home,
+          color: Colors.grey,
+          size: 50,
+        ),
+        Icon(
+          Icons.home,
+          color: Colors.grey,
+          size: 50,
+        ),
+        Icon(
+          Icons.home,
+          color: Colors.grey,
+          size: 50,
+        ),
+      ],
+    );
+  }
+}
+
+class TaskInformation extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.orange[600],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                topLeft: Radius.circular(25),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 20,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 2,
+                    child: Row(
+                      children: [
+                        Text(
+                          'Task name', //HARDCODED
+                          style: TextStyle(
+                            fontFamily: '',
+                            fontSize: 25, //HARDCODED
+                          ),
+                        ),
+                        SizedBox(width: 150), //HARDCODED
+                        Spacer(),
+                        Text(
+                          'priority',
+                          style: TextStyle(
+                              fontFamily: '',
+                              fontSize: 15), //HARDCODED
+                        ),
+                      ],
+                    )),
+                     SizedBox(height: 15,),
+                Expanded(
+                  flex: 4,
+                  child: Align(alignment: Alignment.topLeft ,child: Text(
+                      'lorem ipsume fhjkasfjhnajhfjshnjcnasndjncansjncnajsndj',
+                      style: TextStyle(
+                        fontFamily: '',
+                        fontSize: 15,
+                      ),
+                     
+                      ),
+                      )
+                    
+                ),
+                Divider(height: 20),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.timelapse),
+                        SizedBox(width:20),
+                        Text('xx/xx/xxxx'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class LowerSegmentTitle extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //Text row type of task
+      children: [
+        Text(
+          'Recently Assigned', //HARDCODED
+          style: TextStyle(
+            fontFamily: '',
+            fontSize: 25, //HARDCODED
+          ),
+        ),
+        SizedBox(width: 150), //HARDCODED
+        Spacer(),
+        Text(
+          'All tasks',
+          style: TextStyle(fontFamily: '', fontSize: 15), //HARDCODED
+        ),
+      ],
     );
   }
 }
