13d572cd279076d133d507b7180807891846be5a
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-12-02 15:28:57 +0100

    Set base for firebase utility

--- gi_social_app/lib/home.dart
+++ gi_social_app/lib/home.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
     return Scaffold(
-
-      
-
-
-
+      appBar: AppBar(
+        title: Text('Genshin Impact Social'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('hello')),
+        ],
+      ),
     );
   }
 }
--- gi_social_app/lib/main.dart
+++ gi_social_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'home.dart';
-
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
