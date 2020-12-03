444fdfc976b25ceb15b4063cb9613c2fb9c52590
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2020-12-03 18:04:50 +0100

    Drawer and Post design/programing
    Co-Authored-By: Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>

--- gi_social_app/lib/Widgets/widgets.dart
+++ gi_social_app/lib/Widgets/widgets.dart
@@ -3,35 +3,64 @@ import 'package:flutter/material.dart';
 class PreviewPost extends StatelessWidget {
   // This widget is the root of your application.
   Icon userAvatar;
+  String nickname = 'Nickname';
   String userName = 'User name';
+  String title = 'title';
+  String description =
+      'hola que tal me gustaria ir a hcer unos bosses bien calentittos para conseguir materiales par xiangling';
   List<Icon> descriptionIcons;
   Image descriptionPic;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.grey,
+      color: Colors.white,
       child: Container(
         padding: EdgeInsets.all(20),
         height: 275,
-        width: 350,
+        width: 375,
         child: Column(
           children: [
             Row(
               children: [
                 Container(
                     decoration: BoxDecoration(
-                    
-                      shape: BoxShape.circle
-
-                    ),
+                        color: Colors.grey, shape: BoxShape.circle),
                     child: Icon(
                       Icons.person,
-                      size: 30,
+                      size: 40,
                     )),
-                Text('$userName'),
+                SizedBox(width: 15),
+                Column(
+                  children: [
+                    Text(
+                      '$nickname',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    Text(
+                      '@$userName',
+                      style: TextStyle(color: Colors.blue[600]),
+                    ),
+                  ],
+                ),
               ],
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Text('TITLE', style: TextStyle(color: Colors.black))),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '$description',
+              style: TextStyle(color: Colors.black),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          
           ],
         ),
       ),
--- gi_social_app/lib/home.dart
+++ gi_social_app/lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Widgets/widgets.dart';
+import 'Screens/SettingsScreen.dart';
 
 class HomeScreen extends StatelessWidget {
   // This widget is the root of your application.
@@ -11,6 +12,44 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Genshin Impact Social'),
       ),
+     drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+                accountName: Text('GeoDaddy'),
+                accountEmail: Text('iwillhaveorder@liyue.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://cdn.mos.cms.futurecdn.net/MN988hPEYu9xwrsQ3uikyT.jpg'),
+                ),
+              ),
+              new ListTile(
+                title: Text('Settings'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => SettingsScreen()));
+                },
+              ),
+              new Divider(
+                color: Colors.black,
+                height: 5.0,
+              ),
+              new ListTile(
+                title: Text('low Settings'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => SettingsScreen()));
+                },
+              ),
+            ],
+          ),
+        ),
       body: StreamBuilder(
           stream:
               db.collection('users').doc('Il5ociGM5DEzofBhbdYB').snapshots(),
@@ -18,7 +57,7 @@ class HomeScreen extends StatelessWidget {
             if (!snapshot.hasData) {
               return Center(child: CircularProgressIndicator());
             }
-          final user=snapshot.data; 
+            final user = snapshot.data;
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
